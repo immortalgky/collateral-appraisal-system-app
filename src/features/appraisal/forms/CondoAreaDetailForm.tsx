dc1fbd5e00e7f36902153ca8455bb0f28a9cdaab
thunnuht <thunyathep2544@gmail.com>
2026-02-23 14:57:39 +0700

    Condo area detail field name and total building area field

--- src/features/appraisal/forms/CondoAreaDetailForm.tsx
+++ src/features/appraisal/forms/CondoAreaDetailForm.tsx
@@ -1,35 +1,24 @@
 import FormTable from '@/features/request/components/tables/FormTable';
-import Input from '@/shared/components/Input';
 import type { AreaDetailDtoType } from '@/shared/schemas/v1';
-import { useController, useFormContext, useWatch } from 'react-hook-form';
+import { useEffect } from 'react';
+import { useFormContext, useWatch } from 'react-hook-form';
 
 interface CondoAreaDetailFormProps {
   name: string;
 }
 function CondoAreaDetailForm({ name }: CondoAreaDetailFormProps) {
-  const { control } = useFormContext();
-  const {
-    field,
-    fieldState: { error },
-  } = useController({ name: name, control });
+  const { setValue } = useFormContext();
 
   const properties = useWatch({ name: `${name}` });
   const totalArea = calcTotalArea(properties);
 
+  useEffect(() => {
+    setValue('totalBuildingArea', totalArea);
+  }, [totalArea, setValue]);
+
   return (
     <div className="col-span-12 border-2 rounded-2xl border-gray-100">
       <FormTable headers={propertiesTableHeader} name={`${name}`} />
-      <div className="px-6 pb-6">
-        <Input
-          type="number"
-          {...field}
-          label="Total Area (Sq. M.)"
-          value={totalArea}
-          error={error?.message}
-          readOnly
-          disabled
-        />
-      </div>
     </div>
   );
 }
